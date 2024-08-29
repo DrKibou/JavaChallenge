c20ba0d1122e280aa32ed5c0e512957746143bcd
The Starter <133458932+DrKibou@users.noreply.github.com>
2024-08-29 22:45:36 +0800

    added java string challenge

---
+++ javaString/Solution.java
@@ -0,0 +1,42 @@
+package javaString;
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        int sum = A.length() + B.length(); //sums the length of the strings
+        String isGreater = ""; //holds the result whether the A is lexicographically greater than B 
+        String aUpperCase = "" + A.charAt(0); //separated first letter of the strings
+        String bUpperCase = "" + B.charAt(0);
+        String aString = ""; //holds the rest of the element of the strings
+        String bString = "";
+
+        for(int i = 0; i < A.length(); i++){ //compare each index and return if greater than or not
+            if (A.charAt(i) != B.charAt(i)){
+                int asciiA = (int) A.charAt(i); //convert to ascii
+                int asciiB = (int) B.charAt(i);       
+                isGreater = (asciiA > asciiB) ? "Yes" : "No";
+                break;
+            } else {
+                isGreater = "No";
+            }
+        }
+        for(int i = 1; i < A.length(); i++){ //concantenate the second to the last element of the strings
+            aString += A.charAt(i);
+        }
+        for(int i = 1; i < B.length(); i++){
+            bString += B.charAt(i);
+        }
+        
+        System.out.println(sum + "\n" +isGreater + "\n" + aUpperCase.toUpperCase() + aString +" "+ bUpperCase.toUpperCase() + bString); //print result
+        
+    }
+}
+
+
+
